638e2c4380f3976e66fc8c17a7d3ba7febb6043b
maxime-billon-grand <maxime.billon-grand@laplateforme.io>
2022-09-28 13:08:01 +0200

    Ajout du Job 06

---
+++ Job 06/my_calculator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+case "$2" in 
+	"+" )
+		result=$(($1 + $3));;
+	"-" )
+		result=$(($1 - $3));;
+	"x" | "*" | "X" )
+		result=$(($1 * $3));;
+	"/" | "÷" )
+		result=$(($1 / $3));;
+    * )
+        echo "Vous n'avez pas rentré le bon opérateur !"
+esac
+
+echo $result
+
