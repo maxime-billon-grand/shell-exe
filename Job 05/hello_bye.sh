6fff51dfc7a2f302e7ec11ba8bb2922c02f82bd6
maxime-billon-grand <maxime.billon-grand@laplateforme.io>
2022-09-28 11:10:03 +0200

    Ajout du Job 05

---
+++ Job 05/hello_bye.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$1" = "Hello" ]; then
+	echo "Bonjour, je suis un script !"
+elif [ "$1" = "Bye" ]; then
+	echo "Au revoir et bonne journée !"
+else 
+	echo "Vous n'avez pas rentré le bon paramètre!"
+fi
+
